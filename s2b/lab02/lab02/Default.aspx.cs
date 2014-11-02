407c25a66ce49fa0cea2cb74483bdbbc551fb3e7
Jean Farias Roldão <jeanroldao@gmail.com>
2014-11-02 16:42:20 -0200

    Lab 02
    Signed-off-by: Jean Farias Roldão <jeanroldao@gmail.com>

---
+++ s2b/lab02/lab02/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lab02
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DateTime[] dt = new DateTime[2];
+            int iDate;
+            dt[0] = new DateTime(2002, 5, 1);
+            dt[1] = new DateTime(2002, 6, 1);
+            for (iDate = 0; iDate < 2; iDate++)
+            {
+                Response.Write("Indice = " + iDate + " & Data = " + dt[iDate].ToShortDateString() + "<br>");
+            }
+
+
+        }
+    }
+}
