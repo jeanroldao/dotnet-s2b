592439f50fd8003e8c33865e0fe61f22f6d098f2
Jean Farias Roldão <jeanroldao@gmail.com>
2014-11-16 10:23:33 -0200

    lab3
    Signed-off-by: Jean Farias Roldão <jeanroldao@gmail.com>

---
+++ s2b/Lab3/Lab3.ClassLibrary/ContaCorrente.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3.ClassLibrary
+{
+    public class ContaCorrente
+    {
+        private string nome;
+        private decimal saldo;
+        private DateTime dataCriacao;
+        private DateTime dataUltimoMovimento;
+
+        public string Nome
+        {
+            get { return nome; }
+        }
+
+        public decimal Saldo
+        {
+            get { return saldo; }
+        }
+
+        public DateTime DataCriacao { get { return dataCriacao; } }
+        public DateTime DataUltimoMovimento { get { return dataUltimoMovimento; } }
+
+        public ContaCorrente(string nome) : this(nome, 0M)
+        {
+        }
+
+        public ContaCorrente(string nome, decimal saldoInicial)
+        {
+
+            dataCriacao = DateTime.Now;
+            dataUltimoMovimento = DateTime.Now;
+
+            this.nome = nome;
+            this.saldo = saldoInicial;
+        }
+
+        public void Depositar(decimal val)
+        {
+            dataUltimoMovimento = DateTime.Now;
+            saldo = saldo + val;
+        }
+
+        public virtual void Sacar(decimal val)
+        {
+            dataUltimoMovimento = DateTime.Now;
+            if (val <= saldo)
+                saldo = saldo - val;
+        }
+
+
+    }
+
+    public class ContaCorrente2 : ContaCorrente 
+    {
+        public ContaCorrente2(string nome) : base(nome)
+        {
+            
+        }
+        public ContaCorrente2(string nome, decimal v) : base(nome, v)
+        {
+            
+        }
+        public override void Sacar(decimal val)
+        {
+            throw new Exception("não implementado");
+        }
+    }
+}
---
+++ s2b/Lab3/Lab3/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Lab3.ClassLibrary;
+
+namespace Lab3
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ContaCorrente minhaConta = new ContaCorrente2("Gerson");
+            Response.Write("Movimentações conta: " + minhaConta.Nome + " criada em " + minhaConta.DataCriacao.ToShortDateString() + "<br/>");
+            Response.Write(minhaConta.Saldo + " - " + minhaConta.DataUltimoMovimento.ToShortDateString() + "<br/>");
+            minhaConta.Depositar(100);
+            Response.Write(minhaConta.Saldo + " - " + minhaConta.DataUltimoMovimento.ToShortDateString() + "<br/>");
+            minhaConta.Sacar(50);
+            Response.Write(minhaConta.Saldo + " - " + minhaConta.DataUltimoMovimento.ToShortDateString() + "<br/>");
+
+        }
+    }
+}
